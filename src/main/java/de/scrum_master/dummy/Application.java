081f11e66e030230102b326bd0e4193a6aa7d859
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-03-27 15:38:46 +0700

    Initial commit

---
+++ src/main/java/de/scrum_master/dummy/Application.java
@@ -0,0 +1,19 @@
+package de.scrum_master.dummy;
+
+import sun.security.x509.X509CertInfo;
+
+import java.security.cert.CertificateParsingException;
+
+/**
+ * https://github.com/mojohaus/aspectj-maven-plugin/issues/139
+ */
+public class Application {
+  public static void main(String[] args) {
+    try {
+      new X509CertInfo(new byte[0]);
+    }
+    catch (CertificateParsingException e) {
+      System.out.println(e);
+    }
+  }
+}
